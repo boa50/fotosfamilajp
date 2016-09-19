d36b573f3cd42bfc965e086a78ce5ac7db4aa09c
boa50 <bruno.albuquerque50@gmail.com>
2016-09-19 18:17:20 -0300

    alteração para a busca das propriedades

--- src/controller/ConexaoController.java
+++ src/controller/ConexaoController.java
@@ -2,9 +2,11 @@ package controller;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Properties;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -14,11 +16,33 @@ import com.google.gson.JsonObject;
 
 public class ConexaoController {
 	
-	private final String clientID = "49386168107-su0jlmpro557333i2o0uj18ct08ui8ub.apps.googleusercontent.com";
-	private final String clientSecret = "_kZ0CC0vVlQ8FZSVHXNAojos";
-	private final String refreshToken = "1/ZOSMZLzbPWEIJyf1DrqivzZoLATShsAzlWZA61WU8IcMEudVrK5jSpoR30zcRFq6";
+	private Properties codigos;
+	private InputStream input;
 	
-	private PicasawebService myService = new PicasawebService("FotosFamiliaJP");
+	private String clientId;
+	private String clientSecret;
+	private String refreshToken;
+	
+	private PicasawebService myService;
+	
+	public ConexaoController(){
+		super();
+		this.codigos = new Properties();
+		try {
+			this.input = this.getClass().getClassLoader().getResourceAsStream("codigos.properties");
+			codigos.load(input);
+			
+			this.clientId = codigos.getProperty("clientId");
+			this.clientSecret = codigos.getProperty("clientSecret");
+			this.refreshToken = codigos.getProperty("refreshToken");
+			
+			input.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		this.myService = new PicasawebService("FotosFamiliaJP");
+	}
 	
 	public PicasawebService retornaServico(){
 		try {
@@ -26,7 +50,7 @@ public class ConexaoController {
         	HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         	connection.setRequestMethod("POST");
         	connection.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
-        	String urlParameters = "client_id=" + clientID + "&" +
+        	String urlParameters = "client_id=" + clientId + "&" +
         	         "client_secret=" + clientSecret + "&" +
         	         "refresh_token=" + refreshToken + "&" +
         	         "grant_type=refresh_token";
@@ -47,7 +71,7 @@ public class ConexaoController {
     	
 	    	GoogleCredential credential = new GoogleCredential.Builder().setTransport(new NetHttpTransport())
 	    			.setJsonFactory(new JacksonFactory())
-	    			.setClientSecrets(clientID, clientSecret)
+	    			.setClientSecrets(clientId, clientSecret)
 	    			.build()
 	    			.setAccessToken(json.get("access_token").toString());
 	    	
