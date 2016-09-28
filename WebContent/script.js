5a42bffe811d6681aeea14b954b2e1ff7a1dac55
boa50 <bruno.albuquerque50@gmail.com>
2016-09-28 19:16:31 -0300

    versão inicial com login

--- WebContent/script.js
+++ WebContent/script.js
@@ -1,15 +1,65 @@
-angular.module('fotosFamiliaJpModule', ['ngRoute', 'ui.bootstrap'])
+var albuns = 0;
+var autenticado = false;
+
+angular.module('fotosFamiliaJpModule', ['ngRoute', 'ui.bootstrap', 'ui.navbar'])
+
+.controller('LoginController', function($scope, $http, $location){
+	
+	document.getElementsByTagName("body")[0].className += ' navbar-escondida';
+	
+	$scope.login = function(){
+		$scope.dataLoading = true;
+		
+		if( ($scope.inLogin == 'a') && ($scope.inSenha == 'a')){
+			autenticado = true;
+			document.getElementsByTagName("body")[0].className = 'ng-scope';
+			$location.path('/album');
+		}
+		
+	}
+	
+	
+})
+
+
+.controller('AlbunsController', function($scope, $http){
+	
+	urlTeste = 'http://localhost:8080/fotosfamiliajp/rs/albuns';
+	
+	$scope.retornar = function(){
+		$http.get(urlTeste).success(function(lista){
+			$scope.albuns = lista;
+			albuns = lista;
+			console.log('Lista de albuns preenchida');
+		}).error(function(erro){
+			alert(erro);
+		})
+	}
+	
+	if(albuns == 0){
+		$scope.retornar();
+	}else{
+		$scope.albuns = albuns;
+	}
+		
+})
 
 .controller('FotosController', function($scope, $http, $routeParams){
 	
-	if($routeParams.albumId)
-		urlTeste = 'http://localhost:8080/fotosfamiliajp/rs/fotos/' + $routeParams.albumId;
-	else
-		urlTeste = 'http://localhost:8080/fotosfamiliajp/rs/albuns';
+	urlTeste = 'http://localhost:8080/fotosfamiliajp/rs/fotos/' + $routeParams.albumId;
+	
+	
+	for(var i = 0; i < albuns.length; i++){
 		
+		if(albuns[i].id == $routeParams.albumId)
+			$scope.nomeAlbum = albuns[i].nome;
+		
+	}
+		
+	
 	$scope.retornar = function(){
 		$http.get(urlTeste).success(function(lista){
-			$scope.lista = lista;
+			$scope.fotos = lista
 		}).error(function(erro){
 			alert(erro);
 		})
@@ -21,9 +71,13 @@ angular.module('fotosFamiliaJpModule', ['ngRoute', 'ui.bootstrap'])
 
 .config(function($routeProvider){
 	$routeProvider
+		.when('/login', {
+			templateUrl: 'login.html',
+			controller: 'LoginController'
+		})
 		.when('/album', {
 			templateUrl: 'album.html',
-			controller: 'FotosController'
+			controller: 'AlbunsController'
 		})
 		.when('/album/:albumId', {
 			templateUrl: 'foto.html',
