5ebca2f422e449fd9b2198bfc9fc931030fb5173
boa50 <bruno.albuquerque50@gmail.com>
2016-10-02 11:32:14 -0300

    Correções: layout-xs, back button, menu galeria

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,6 +1,23 @@
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
 var albuns = 0;
 var acesso = "";
+//TODO alerar para algo melhor
+//Desenvolvimento
+var urlBase = "http://localhost:8080/fotosfamiliajp";
+//Produção
+//var urlBase = "http://familiajp-boa50.rhcloud.com";
+
+window.onhashchange = function(){
+	if( (window.location.hash == '#/album') 
+		&& (document.getElementById('blueimp-gallery').style.display == 'block') ){
+		document.getElementById('close').click();
+		//TODO alterar para ficar no mesmo canto da página
+		window.history.forward();
+	}
+	if(window.location.hash == '#/login'){
+		window.history.forward();
+	}
+}
 
 angular.module('fotosFamiliaJpModule', ['ngRoute', 'ui.bootstrap', 'ui.navbar'])
 
@@ -19,12 +36,13 @@ angular.module('fotosFamiliaJpModule', ['ngRoute', 'ui.bootstrap', 'ui.navbar'])
 
 .controller('LoginController', function($scope, $http, $location){
 	
-	document.getElementsByTagName("body")[0].className += ' navbar-escondida';
+	if(this.acesso == '')
+		document.getElementsByTagName("body")[0].className += ' navbar-escondida';
 	
 	$scope.login = function(){
 		$scope.dataLoading = true;
 		
-		url = 'http://localhost:8080/fotosfamiliajp/rs/login/' 
+		url = urlBase + '/rs/login/' 
 			+ Base64.encode($scope.inLogin) + '/' 
 			+ Base64.encode($scope.inSenha);
 		
@@ -53,7 +71,7 @@ angular.module('fotosFamiliaJpModule', ['ngRoute', 'ui.bootstrap', 'ui.navbar'])
 
 .controller('AlbunsController', function($scope, $http){
 	
-	url = 'http://localhost:8080/fotosfamiliajp/rs/albuns/' + acesso;
+	url = urlBase + '/rs/albuns/' + acesso;
 	
 	$scope.retornar = function(){
 		$http.get(url).success(function(lista){
@@ -75,10 +93,11 @@ angular.module('fotosFamiliaJpModule', ['ngRoute', 'ui.bootstrap', 'ui.navbar'])
 		
 })
 
-.controller('FotosController', function($scope, $http, $routeParams){
+.controller('FotosController', function($scope, $http, $routeParams, $location){
 	
-	url = 'http://localhost:8080/fotosfamiliajp/rs/fotos/' + $routeParams.albumId + '/' + acesso;
+	url = urlBase + '/rs/fotos/' + $routeParams.albumId + '/' + acesso;
 	
+	var albumId = $routeParams.albumId; 
 	
 	for(var i = 0; i < albuns.length; i++){
 		if(albuns[i].id == $routeParams.albumId)
